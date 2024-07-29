7a3738dabe8d02fc50a2e9167e258272c4a77faf
Gremious Grenald <gremious@gremy.co.uk>
2024-07-29 13:20:06 +0100

    Trouble/flash

--- lua/plugins/install.lua
+++ lua/plugins/install.lua
@@ -138,7 +138,7 @@ require("lazy").setup({
 				-- desc = "Flash Treesitter",
 			-- },
 			{
-				"r",
+				"<leader>r",
 				mode = "o",
 				function()
 					require("flash").remote()
@@ -193,6 +193,43 @@ require("lazy").setup({
 		config = function()
 		   vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#ffcb6b", underline = true })
 		end
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			-- {
+				-- "<leader>xX",
+				-- "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				-- desc = "Buffer Diagnostics (Trouble)",
+			-- },
+			-- {
+				-- "<leader>cs",
+				-- "<cmd>Trouble symbols toggle focus=false<cr>",
+				-- desc = "Symbols (Trouble)",
+			-- },
+			-- {
+				-- "<leader>cl",
+				-- "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				-- desc = "LSP Definitions / references / ... (Trouble)",
+			-- },
+			-- {
+				-- "<leader>xL",
+				-- "<cmd>Trouble loclist toggle<cr>",
+				-- desc = "Location List (Trouble)",
+			-- },
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
 	},
 
 	-- ==/ themes /==
