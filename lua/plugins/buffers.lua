0f2796337aaa65fa4b578661ffe8937027987ebe
Gremious Grenald <gremious@gremy.co.uk>
2026-04-10 20:06:37 +0100

    Nuke

--- lua/plugins/buffers.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- Scopes/hides buffers to vim tabs, :bnext and :bprev are now workspaces basically
-	{
-		"tiagovla/scope.nvim",
-		opts = { restore_state = false },
-	},
-	-- Don't close the whole tab/window on :bd - use :BD instead
-	"qpkorr/vim-bufkill",
-}
--- lua/plugins/text/highlighting/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{ "fladson/vim-kitty", branch = "main" }, -- kitty config highlighting
-	"imsnif/kdl.vim", -- kdl highlighting
-	"vmchale/dhall-vim", -- dhall highlighting
-	"ron-rs/ron.vim", -- ron highlighting
-	"GutenYe/json5.vim", -- json5 highlighting
-}
